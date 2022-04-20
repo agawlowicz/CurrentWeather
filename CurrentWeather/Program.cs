7bb827a8fb5d628e94ec9dcb416e5d6d5c9a0411
Amanda Gawlowicz <agawlowicz07@gmail.com>
2022-04-20 10:38:56 -0500

    using API, give temperatures and weather, pre-review

---
+++ CurrentWeather/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace CurrentWeather
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = File.ReadAllText("appsettings.json");
+
+            // We want this to be private and ignored from git
+            var apiKey = JObject.Parse(key).GetValue("DefaultKey").ToString();
+
+            Console.WriteLine("Enter zip code: ");
+            var zipCode = Console.ReadLine();
+
+            var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&units=imperial&appid={apiKey}";
+
+            var client = new HttpClient();
+
+            //Call API
+            var weather = client.GetStringAsync(weatherURL).Result;
+
+            //Get data
+            var cityName = JObject.Parse(weather)["name"];
+
+            //var weatherSky = JObject.Parse(weather).GetValue("main").ToString(); //need to get "main" from "weather"
+
+            var temp = double.Parse(JObject.Parse(weather)["main"]["temp"].ToString());
+
+            var feelsLike = double.Parse(JObject.Parse(weather)["main"]["feels_like"].ToString());
+
+            var minTemp = double.Parse(JObject.Parse(weather)["main"]["temp_min"].ToString());
+            var maxTemp = double.Parse(JObject.Parse(weather)["main"]["temp_max"].ToString());
+
+            Console.WriteLine($"Current weather in {cityName}:\n");
+            //Console.WriteLine($"It's {temp} degrees Fahrenheit and {weatherSky}.\n");
+            //Console.WriteLine($"Temperature: {temp} degrees Fahrenheit");
+            Console.WriteLine($"Today's high is {maxTemp} with a low of {minTemp}.\n");
+
+            Console.WriteLine($"It feels like {feelsLike} degrees out.");
+
+        }
+    }
+}
